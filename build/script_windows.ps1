e2af859cc8da7c74facedb012a5456b183904fd2
confusdcodr <confusdcodr@users.noreply.github.com>
2019-01-23 13:36:14 -0500

    BS

--- build/script_windows.ps1
+++ build/script_windows.ps1
@@ -1,4 +1,3 @@
-#Start-Service salt-minion
 Start-Process "C:\salt\salt-call.bat --local test.ping"
 Start-Process "C:\salt\salt-call.bat --local state.show_sls amazon-inspector"
 Start-Process "C:\salt\salt-call.bat --local state.sls amazon-inspector mock=True"
